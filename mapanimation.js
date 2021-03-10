f15aa50e8f59d057c715b6c1705bd81b6ea8ed43
Shaiah Wren <sde2111@columbia.edu>
2021-03-09 20:26:01 -0500

    add animaitons

--- mapanimation.js
+++ mapanimation.js
@@ -1,3 +1,16 @@
+
+mapboxgl.accessToken =
+"pk.eyJ1Ijoic2Ftc3dhcmluZ2VuIiwiYSI6ImNrbHh0cnV5ZDJ1cG0yb2x3MTRsOXVjZ2sifQ.UMlM0h2TgOuOddV2X73ewQ";
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.104081,42.365554],
+    zoom: 14
+});
+
+
+
 async function run(){
     // get bus data    
 	const locations = await getBusLocations();
